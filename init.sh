2623746d08109f1abdfc6a63c9630f7bb6d3eccc
Bruno Faria <brunocfnba@gmail.com>
2019-04-29 15:26:34 -0400

    Added exception handling to python and shell

--- init.sh
+++ init.sh
@@ -2,4 +2,9 @@
 
 python3 $SPARK_HOME/set_pwd.py $JUPYTER_PWD
 
+ret=$?
+if [ $ret -ne 0 ]; then
+     exit 1
+fi
+
 jupyter notebook --ip=0.0.0.0 --no-browser --allow-root
--- set_pwd.py
+++ set_pwd.py
@@ -2,8 +2,10 @@ import sys
 from notebook.auth import passwd
 
 if len(sys.argv) < 2:
+    raise Exception("You must define a password")
     sys. exit("You must define a password")
 elif len(sys.argv[1]) < 8:
+    raise Exception("Password must have at least 8 characters!")
     sys.exit("Password must have at least 8 characters!")
 
 pwd = passwd(sys.argv[1])
